feccbbe8a146d622081b269236b59a4b428bf4d4
Attiya Touqir <attiyatouqir2@gmail.com>
2026-03-24 23:30:56 +0500

    Updated Lab1 file

--- Lab1.java
+++ Lab1.java
@@ -1,5 +1,5 @@
 public class Lab1 {
-    public static void main(String[] args) {
-        System.out.println("This is first java program for SCD Lab by Attiya Touqir.");
-    }
+  public static void main(String[] args) {
+    System.out.println("This is updated java program for SCD Lab by Attiya Touqir.");
+  }
 }
