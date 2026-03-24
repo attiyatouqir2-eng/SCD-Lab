1edc464051076d4e76433e794c9842ed04e3b309
Attiya Touqir <attiyatouqir2@gmail.com>
2026-03-24 23:20:34 +0500

    Students and Main

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    Students s1 = new Students();
+    s1.name = "Attiya Touqir";
+    s1.rollNumber = 5028;
+    s1.displayInfo();
+
+    Students s2 = new Students();
+    s2.name = "Shehzana Touqir";
+    s2.rollNumber = 5001;
+    s2.displayInfo();
+
+  }
+}
